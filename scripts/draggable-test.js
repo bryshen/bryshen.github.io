5efd65fb29da167c68b1b99de79e928009f1b804
Bryan <shenbbryan@gmail.com>
2022-12-30 01:06:09 -0500

    drop effect

--- scripts/draggable-test.js
+++ scripts/draggable-test.js
@@ -1,69 +1,110 @@
-  //create class for a draggable div
-  class DraggableDiv {
-    constructor(color, x, y) {
-      //create div element and set properties
-      this.div = document.createElement("div");
-      this.div.style.backgroundColor = color;
-      this.div.style.width = "100px";
-      this.div.style.height = "100px";
-      this.div.style.position = "absolute";
-      this.div.style.top = y + "px";
-      this.div.style.left = x + "px";
-      this.div.draggable = true;
-      //add drag start event listener
-      this.div.addEventListener("dragstart", () => {
-        this.dragStart();
-      });
-      //add drag end event listener
-      this.div.addEventListener("dragend", () => {
-        this.dragEnd();
-      });
+// Define the class that creates a draggable div
+class DraggableDiv {
+    constructor(id, color) {
+      this.id = id;
+      this.color = color;
+      this.element = document.createElement('div');
+      this.element.id = id;
+      this.element.style.backgroundColor = color;
+      this.element.style.position = 'absolute';
+      this.element.style.width = '100px';
+      this.element.style.height = '100px';
+      document.body.appendChild(this.element);
     }
-    //drag start event handler
-    dragStart() {
-      this.div.style.opacity = 0.5;
+  
+    // Method that makes the div draggable
+    makeDraggable() {
+      this.element.addEventListener('mousedown', this.dragStart.bind(this));
+      document.addEventListener('mousemove', this.drag.bind(this));
+      document.addEventListener('mouseup', this.dragEnd.bind(this));
+      this.element.addEventListener('touchstart', this.dragStart.bind(this));
+      document.addEventListener('touchmove', this.drag.bind(this));
+      document.addEventListener('touchend', this.dragEnd.bind(this));
     }
-    //drag end event handler
-    dragEnd() {
-      this.div.style.opacity = 1;
-      //get coordinates of the draggable div
-      var x = this.div.offsetLeft;
-      var y = this.div.offsetTop;
-      //check if it is over any of the static divs
-      for (let i = 0; i < 3; i++) {
-        var div = document.getElementById("static" + I);
-        //get coordinates of each static div
-        var x0 = div.offsetLeft;
-        var y0 = div.offsetTop;
-        var x1 = x0 + div.offsetWidth;
-        var y1 = y0 + div.offsetHeight;
-        //check if the draggable div is within the boundaries of the static div
-        if (x > x0 && x < x1 && y > y0 && y < y1) {
-          //if yes, change the color of the static div and remove the draggable div
-          div.style.backgroundColor = this.div.style.backgroundColor;
-          this.div.remove();
+  
+    // Method that controls what happens when the draggable div is clicked/tapped
+    dragStart(e) {
+      this.startX = e.pageX || e.touches[0].pageX;
+      this.startY = e.pageY || e.touches[0].pageY;
+      this.startLeft = this.element.offsetLeft;
+      this.startTop = this.element.offsetTop;
+      this.dragging = true;
+    }
+  
+    // Method that controls what happens when the draggable div is dragged
+    drag(e) {
+      if (this.dragging) {
+        let currentX = e.pageX || e.touches[0].pageX;
+        let currentY = e.pageY || e.touches[0].pageY;
+        let left = (this.startLeft + currentX - this.startX);
+        let top = (this.startTop + currentY - this.startY);
+        this.element.style.left = left + 'px';
+        this.element.style.top = top + 'px';
+      }
+    }
+  
+    // Method that controls what happens when the draggable div is released
+    dragEnd(e) {
+      this.dragging = false;
+      // Check if the draggable div has been released over a static div
+      let staticDivs = document.querySelectorAll('.static-div');
+      for (let staticDiv of staticDivs) {
+        let staticDivRect = staticDiv.getBoundingClientRect();
+        let draggableDivRect = this.element.getBoundingClientRect();
+        if (this.checkCollision(staticDivRect, draggableDivRect)) {
+          // If the draggable div has been released over a static div, delete the draggable div and change the color of the static div
+          staticDiv.style.backgroundColor = this.color;
+          document.body.removeChild(this.element);
         }
       }
     }
+  
+    // Method that checks if two divs are colliding
+    checkCollision(rect1, rect2) {
+      if (rect1.x < rect2.x + rect2.width &&
+        rect1.x + rect1.width > rect2.x &&
+        rect1.y < rect2.y + rect2.height &&
+        rect1.height + rect1.y > rect2.y) {
+          return true;
+      }
+      return false;
+    }
   }
-
-  //create static divs
-  for (let i = 0; i < 3; i++) {
-    var div = document.createElement("div");
-    div.style.backgroundColor = "#ccc";
-    div.style.width = "100px";
-    div.style.height = "100px";
-    div.style.position = "absolute";
-    div.style.top = (100 + i * 150) + "px";
-    div.style.left = "100px";
-    div.id = "static" + i;
-    document.body.appendChild(div);
-  }
-
-  //create draggable divs
-  var draggableDiv1 = new DraggableDiv("red", 200, 100);
-  document.body.appendChild(draggableDiv1.div);
-  var draggableDiv2 = new DraggableDiv("green", 200, 250);
-  document.body.appendChild(draggableDiv2.div);
-  var draggableDiv3 = new DraggableDiv("blue", 200, 400);
-  document.body.appendChild(draggableDiv3.div);
+  
+  // Create 3 draggable divs
+  let div1 = new DraggableDiv('draggable-div-1', '#FF0000');
+  div1.makeDraggable();
+  
+  let div2 = new DraggableDiv('draggable-div-2', '#00FF00');
+  div2.makeDraggable();
+  
+  let div3 = new DraggableDiv('draggable-div-3', '#0000FF');
+  div3.makeDraggable();
+  
+  // Create 3 static divs
+  let staticDiv1 = document.createElement('div');
+  staticDiv1.className = 'static-div';
+  staticDiv1.style.position = 'absolute';
+  staticDiv1.style.width = '100px';
+  staticDiv1.style.height = '100px';
+  staticDiv1.style.left = '25px';
+  staticDiv1.style.top = '25px';
+  document.body.appendChild(staticDiv1);
+  
+  let staticDiv2 = document.createElement('div');
+  staticDiv2.className = 'static-div';
+  staticDiv2.style.position = 'absolute';
+  staticDiv2.style.width = '100px';
+  staticDiv2.style.height = '100px';
+  staticDiv2.style.left = '150px';
+  staticDiv2.style.top = '150px';
+  document.body.appendChild(staticDiv2);
+  
+  let staticDiv3 = document.createElement('div');
+  staticDiv3.className = 'static-div';
+  staticDiv3.style.position = 'absolute';
+  staticDiv3.style.width = '100px';
+  staticDiv3.style.height = '100px';
+  staticDiv3.style.left = '275px';
+  staticDiv3.style.top = '275px';
+  document.body.appendChild(staticDiv3);
--- scripts/draggable.js
+++ scripts/draggable.js
@@ -3,6 +3,7 @@ export class Draggable {
     constructor(element) {
         this.element = element;
         this.element.style.position = 'absolute';
+        this.element.classList.add('draggable-item');
         this.isDragging = false;
         document.addEventListener('mousemove', this.onInput.bind(this));
         document.addEventListener('touchmove', this.onInput.bind(this));
@@ -12,7 +13,7 @@ export class Draggable {
         this.touchEnd = this.onTouchEnd.bind(this);
     }
 
-    onInput(event){
+    onInput(event) {
         if (!this.dragging) return;
         this.drag(event.clientX || event.touches[0].clientX, event.clientY || event.touches[0].clientY);
     }
@@ -33,14 +34,12 @@ export class Draggable {
 
     onMouseUp() {
         console.log('no longer dragging item');
-        this.dragging = false;
-        clearInterval(this.dragCheck);
-
         this.element.removeEventListener('mouseup', this.mouseUp);
+        this.dragEnd();
     }
     onTouchEnd() {
-        this.dragging = false;
         this.element.removeEventListener('touchend', this.touchEnd);
+        this.dragEnd();
     }
     startDrag(x, y) {
         console.log('starting drag');
@@ -52,7 +51,7 @@ export class Draggable {
     }
     drag(x, y) {
         if (!this.dragging)
-        return;
+            return;
         console.log('dragging item');
 
         const deltaX = x - this.lastX;
@@ -62,12 +61,57 @@ export class Draggable {
         this.lastX = x;
         this.lastY = y;
     }
+
+    // Method that controls what happens when the draggable div is released
+    dragEnd() {
+        this.dragging = false;
+        // Check if the draggable div has been released over a static div
+        let staticDivs = document.querySelectorAll('.inventory-slot');
+        let mostCoveredDiv = null;
+        let mostCoveredArea = 0;
+        for (let staticDiv of staticDivs) {
+            let staticDivRect = staticDiv.getBoundingClientRect();
+            let draggableDivRect = this.element.getBoundingClientRect();
+            if (this.checkCollision(staticDivRect, draggableDivRect)) {
+                // If the draggable div has been released over a static div, delete the draggable div and change the color of the static div
+                console.log(this.element.style.backgroundColor);
+                let overlapArea = this.calculateOverlapArea(staticDivRect, draggableDivRect);
+                if (overlapArea > mostCoveredArea) {
+                    mostCoveredArea = overlapArea;
+                    mostCoveredDiv = staticDiv;
+                }
+            }
+        }
+        if (mostCoveredDiv != null) {
+            mostCoveredDiv.style.backgroundColor = 'blue';
+            this.element.remove();
+        }
+    }
+
+    calculateOverlapArea(rect1, rect2) {
+        let xOverlap = Math.max(0, Math.min(rect1.x + rect1.width, rect2.x + rect2.width) - Math.max(rect1.x, rect2.x));
+        let yOverlap = Math.max(0, Math.min(rect1.y + rect1.height, rect2.y + rect2.height) - Math.max(rect1.y, rect2.y));
+        return xOverlap * yOverlap;
+    }
+
+    // Method that checks if two divs are colliding
+    checkCollision(rect1, rect2) {
+        if (rect1.x < rect2.x + rect2.width &&
+            rect1.x + rect1.width > rect2.x &&
+            rect1.y < rect2.y + rect2.height &&
+            rect1.height + rect1.y > rect2.y) {
+            return true;
+        }
+        return false;
+    }
+
+
 }
 
 function getCenter(div) {
     var rect = div.getBoundingClientRect();
     return {
-      x: rect.left + rect.width / 2,
-      y: rect.top + rect.height / 2
+        x: rect.left + rect.width / 2,
+        y: rect.top + rect.height / 2
     };
-  }
+}
